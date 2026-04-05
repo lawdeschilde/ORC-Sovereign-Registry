8116a7cb520812e0661c707655a1d690bf1a0651
JuryDuely <lawdeschilde@protonmail.com>
2026-04-05 00:32:41 -0400

    Update ORCToken.sol
    // SPDX-License-Identifier: MIT
    pragma solidity ^0.8.0;
    
    contract ORCSovereign {
        string public name = "ORO Reserve Credit";
        string public symbol = "ORC";
        uint256 public physicalBacking = 1400000000; // $1.4B Security Interest
    
        mapping(address => uint256) public balances;
    
        function executeDrawdown(uint256 amount) public {
            // This is the "Quan" trigger
            balances[msg.sender] += amount;
        }
    }

--- ORCToken.sol
+++ ORCToken.sol
@@ -1,1 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract ORCSovereign {
+    string public name = "ORO Reserve Credit";
+    string public symbol = "ORC";
+    uint256 public physicalBacking = 1400000000; // $1.4B Security Interest
+
+    mapping(address => uint256) public balances;
+
+    function executeDrawdown(uint256 amount) public {
+        // This is the "Quan" trigger
+        balances[msg.sender] += amount;
+    }
+}
 
